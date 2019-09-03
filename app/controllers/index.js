cafd420925cfc45fd01dbb73636983b93a6c981f
core <seorisachs@gmail.com>
2019-09-03 14:07:27 -0400

    controllers and observers

---
+++ app/controllers/index.js
@@ -0,0 +1,31 @@
+// import Controller from '@ember/controller';
+// import {computed, observer} from '@ember/controller';
+
+// export default Controller.extend({
+
+//   isDisabled: true, 
+//   emailAddress: '',
+//   actualEmailAddress: computed('emailAddress', function() {
+//     console.log('actualEmailAddress function is callled:' this.get('emailAddress'))
+//   } )
+
+// });
+
+import { computed, observer } from '@ember/object';
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+  isDisabled: true,
+
+  emailAddress: '',
+
+  actualEmailAddress: computed('emailAddress', function() {
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }),
+
+  emailAddressChanged: observer('emailAddress', function() {
+    console.log('observer is called', this.get('emailAddress'));
+  })
+
+});
